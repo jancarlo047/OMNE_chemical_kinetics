520c67877efceb25e27f8d08aff2bb2f0f00a63f
jancarlo047 <jalvarezc047@outlook.com>
2024-06-03 13:58:21 -0600

    Solo se modificaron nombres de funciones

--- extrafunctions.jl
+++ extrafunctions.jl
@@ -1,9 +1,23 @@
-function check_values_in_range(vector::Vector)
-    if all(0 <= x <= 1 for x in vector)
-        #println("Todos los valores están en el rango [0,1].")
-        return true
-    else
-        #println("No todos los valores están en el rango [0,1].")
-        return false
-    end
+"""
+    check_values_in_range(vector::Vector{Float64})
+
+Revisa si todos los valores en un vector están en el rango [0,1].
+
+# Argumentos
+- `vector::Vector{Float64}`: Un vector de números de punto flotante.
+
+# Devoluciones
+- `Bool`: Devuelve `true` si todos los valores están en el rango [0,1], de lo contrario devuelve `false`.
+
+# Ejemplos
+```julia
+julia> check_values_in_range([0.1, 0.2, 0.3])
+true
+
+julia> check_values_in_range([0.1, 1.2, 0.3])
+false"""
+
+
+function check_values_in_range(vector::Vector{Float64})
+    return all(x -> 0 <= x <= 1, vector)
 end
--- generalfunctions.jl
+++ generalfunctions.jl
@@ -1,5 +1,5 @@
 """
-    sol_diff_eq_ord(f, x₀, t₀, h, N; method="RK4")
+    sol_ODE(f, x₀, t₀, h, N; method="RK4")
 
 Resuelve una ecuación diferencial ordinaria utilizando varios métodos.
 
@@ -15,7 +15,7 @@ Resuelve una ecuación diferencial ordinaria utilizando varios métodos.
 - `x::Vector{Real}`: Vector de soluciones x.
 - `t::Vector{Real}`: Vector de tiempos t.
 """
-function sol_diff_eq_ord(f::Function, x₀::Real, t₀::Real, h::Real, N::Integer; method::String="RK4")
+function sol_ODE(f::Function, x₀::Real, t₀::Real, h::Real, N::Integer; method::String="RK4")
     t = range(t₀, step=h, length=N+1)
     x = zeros(Real, length(t))
     x[1] = x₀
@@ -44,7 +44,7 @@ function sol_diff_eq_ord(f::Function, x₀::Real, t₀::Real, h::Real, N::Intege
 end
 
 """
-    sol_diff_eq_ord(f₁, f₂, x₁₀, x₂₀, t₀, h, N; method="RK4")
+    sol_two_ODE(f₁, f₂, x₁₀, x₂₀, t₀, h, N; method="RK4")
 
 Resuelve dos ecuaciones diferenciales ordinarias simultáneamente.
 
@@ -63,7 +63,7 @@ Resuelve dos ecuaciones diferenciales ordinarias simultáneamente.
 - `x₂`: Solución de la segunda ecuación diferencial.
 - `t`: Puntos de tiempo correspondientes.
 """
-function sol_two_diff_eq_ord(f₁::Function, f₂::Function, x₁₀::Real, x₂₀::Real, t₀::Real, h::Real, N::Integer; method::String="RK4")
+function sol_two_ODE(f₁::Function, f₂::Function, x₁₀::Real, x₂₀::Real, t₀::Real, h::Real, N::Integer; method::String="RK4")
     t = range(t₀, step=h, length=N+1)
     x₁ = zeros(Real, length(t))
     x₂ = zeros(Real, length(t))
@@ -101,6 +101,52 @@ function sol_two_diff_eq_ord(f₁::Function, f₂::Function, x₁₀::Real, x₂
     end
 
     return x₁, x₂, t
+end
+
+"""
+    sol_ODE_param(f, c, x₀, t₀, h, N; method = "RK4")
+
+Resuelve una ecuación diferencial ordinaria de primer orden utilizando varios métodos, considerando un termino de parametros.
+
+# Argumentos
+- `f`: Una función que representa la ecuación diferencial.
+- `c`: Un array que contiene los coeficientes de la ecuación diferencial.
+- `x₀`: La condición inicial para `x`.
+- `t₀`: El tiempo inicial.
+- `h`: El paso de tiempo.
+- `N`: El número de pasos de tiempo.
+- `method`: El método numérico a utilizar. Puede ser "Euler", "MidPoint", "Ralston" o "RK4". Por defecto es "RK4".
+
+# Devoluciones
+- `x`: Un array que contiene la solución `x` en cada paso de tiempo.
+- `t`: Un array que contiene los tiempos correspondientes a cada paso.
+"""
+function sol_ODE_param(f :: Function, c::Array, x₀ :: Real, t₀ :: Real, h :: Real, N :: Integer; method = "RK4")
+	t = collect(t₀:h:t₀ + N*h)
+	x = zeros(length(t))
+	x[1] = x₀
+
+	for n in 2:length(t)
+		if method == "Euler"
+			x[n] = x[n-1] + h*f(t[n-1], x[n-1],c[n-1])
+		elseif method == "MidPoint"
+			x[n] = x[n-1] + h*f(t[n-1] + 0.5*h, x[n-1] + 0.5*h*f(t[n-1], x[n-1],c[n-1]),c[n-1])
+		elseif method == "Ralston"
+			k₁ = f(t[n-1], x[n-1],c[n-1])
+			k₂ = f(t[n-1] + (2/3)*h, x[n-1] + (2/3)*h*k₁,c[n-1])
+			x[n] = x[n-1] + h*(0.25*k₁ + 0.75*k₂)
+		elseif method == "RK4"
+			k₁ = f(t[n-1], x[n-1],c[n-1])
+			k₂ = f(t[n-1] + 0.5*h, x[n-1] + 0.5*h*k₁,c[n-1])
+			k₃ = f(t[n-1] + 0.5*h, x[n-1] + 0.5*h*k₂,c[n-1])
+			k₄ = f(t[n-1] + h, x[n-1] + h*k₃,c[n-1])
+			x[n] = x[n-1] + h*(k₁ + 2*k₂ + 2*k₃ + k₄)/6
+		else
+			error("Método no reconocido: $method")
+		end
+	end
+
+	return x, t
 end
 
 
